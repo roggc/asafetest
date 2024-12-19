c846482d32f7d079abcc19ef2c423917572de7bc
roggc <roggc9@gmail.com>
2024-12-19 15:14:47 +0100

    add lucide icons

--- app/ui/theme-switcher.tsx
+++ app/ui/theme-switcher.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { Sun, Moon } from "lucide-react";
 
 export default function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false);
@@ -12,12 +13,12 @@ export default function ThemeSwitcher() {
 
   if (!mounted) return null;
 
-  return (
-    <button
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="p-2 bg-gray-200 dark:bg-gray-800 rounded"
-    >
-      Cambiar a {theme === "dark" ? "modo claro" : "modo oscuro"}
-    </button>
+  const changeThemeHandler = () =>
+    setTheme(theme === "dark" ? "light" : "dark");
+
+  return theme === "dark" ? (
+    <Sun size={24} onClick={changeThemeHandler} className="cursor-pointer" />
+  ) : (
+    <Moon size={24} onClick={changeThemeHandler} className="cursor-pointer" />
   );
 }
