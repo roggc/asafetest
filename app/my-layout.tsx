d31a8c598f3287c50bc251fb5bb745bdb357b810
roggc <roggc9@gmail.com>
2024-12-19 17:40:38 +0100

    add sidebar

--- app/my-layout.tsx
+++ app/my-layout.tsx
@@ -1,4 +1,6 @@
 import ThemeSwitcher from "@/app/ui/theme-switcher";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 export default function MyLayout({
   children,
@@ -6,39 +8,24 @@ export default function MyLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <header className="h-12 flex items-center justify-between px-4">
+    <div className="flex flex-col h-screen">
+      {/* Encabezado */}
+      <header className="h-12 flex items-center justify-between px-4 bg-[hsl(var(--sidebar-background))] text-[hsl(var(--sidebar-foreground))] border-b border-[hsl(var(--sidebar-border))]">
         <h1 className="text-lg font-semibold">A-SAFE Test App</h1>
         <ThemeSwitcher />
       </header>
-      <div className="flex flex-1">
-        {/* Barra lateral */}
-        <aside className="bg-gray-200 w-64 p-4">
-          <nav>
-            <ul>
-              <li>
-                <a
-                  href="/"
-                  className="block py-2 px-4 rounded hover:bg-gray-300"
-                >
-                  Inicio
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/about"
-                  className="block py-2 px-4 rounded hover:bg-gray-300"
-                >
-                  Acerca de
-                </a>
-              </li>
-              {/* Agrega más enlaces según sea necesario */}
-            </ul>
-          </nav>
-        </aside>
-        {/* Contenido principal */}
-        <main className="flex-1 p-4 overflow-auto">{children}</main>
+      {/* Contenedor Principal */}
+      <div className="flex flex-1 overflow-hidden">
+        <SidebarProvider>
+          {/* Barra Lateral */}
+          <AppSidebar />
+          {/* Contenido Principal */}
+          <main className="flex-1 p-4 overflow-auto">
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
       </div>
-    </>
+    </div>
   );
 }
