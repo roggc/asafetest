f3eb59b860768379c08e435327418bc8c968a02e
roggc <roggc9@gmail.com>
2024-12-20 12:39:51 +0100

    already signed in

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,14 +2,24 @@
 
 import FormCard from "@/app/signin/card";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
 
 export default function SignInPage() {
   const [error, setError] = useState(false);
+  const { status } = useSession();
 
-  return (
-    <div className="flex flex-1 flex-col items-center justify-center h-full">
-      <FormCard setError={setError} />
-      {error && <p className="text-red-500 mt-2">Invalid credentials</p>}
-    </div>
-  );
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (status === "unauthenticated") {
+    return (
+      <div className="flex flex-1 flex-col items-center justify-center h-full">
+        <FormCard setError={setError} />
+        {error && <p className="text-red-500 mt-2">Invalid credentials</p>}
+      </div>
+    );
+  }
+
+  return <p className="text-center">You are already signed in</p>;
 }
