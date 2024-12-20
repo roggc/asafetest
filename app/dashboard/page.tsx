8aa28503b3797881dd7019bcc855d0ca8c75927b
roggc <roggc9@gmail.com>
2024-12-20 12:33:10 +0100

    handle navigation links

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default async function Dashboard() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    redirect("/signin");
+    redirect("/");
   }
 
   return (
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,4 +1,6 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+"use client";
+
+import { Home, Gauge } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
@@ -8,41 +10,46 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
-const items = [
-  { title: "Home", url: "#", icon: Home },
-  { title: "Inbox", url: "#", icon: Inbox },
-  { title: "Calendar", url: "#", icon: Calendar },
-  { title: "Search", url: "#", icon: Search },
-  { title: "Settings", url: "#", icon: Settings },
+const availableItems = [
+  { title: "Home", url: "/", icon: Home },
+  { title: "Dashboard", url: "/dashboard", icon: Gauge },
 ];
 
 export function AppSidebar() {
+  const { status } = useSession();
+  const items =
+    status === "authenticated" ? availableItems : availableItems.slice(0, 1);
+
   return (
-    // <aside className="w-64 h-full bg-gray-200 overflow-y-auto">
     <Sidebar className="top-[--header-height]" collapsible="icon">
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a
-                      href={item.url}
-                      className="flex items-center space-x-2 p-2 hover:bg-gray-300 rounded"
-                    >
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {status === "loading" ? (
+                <p>Loading...</p>
+              ) : (
+                items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        href={item.url}
+                        className="flex items-center space-x-2 p-2 hover:bg-gray-300 rounded"
+                      >
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))
+              )}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
-    // </aside>
   );
 }
