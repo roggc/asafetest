e405b4ac898a81c6a61ac68e90186669a9b98834
roggc <roggc9@gmail.com>
2024-12-20 12:13:02 +0100

    handle session

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,11 +2,12 @@
 
 import { ThemeProvider } from "next-themes";
 import { PropsWithChildren } from "react";
+import AuthProvider from "@/app/auth-provider";
 
 export function Providers({ children }: PropsWithChildren<{}>) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
+      <AuthProvider>{children}</AuthProvider>
     </ThemeProvider>
   );
 }
