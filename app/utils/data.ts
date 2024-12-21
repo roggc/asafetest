a9038ceb42f55ef426145d28b82c044dd6c6e5ba
roggc <roggc9@gmail.com>
2024-12-21 02:12:37 +0100

    dashboard

---
+++ app/utils/data.ts
@@ -0,0 +1,36 @@
+export async function fetchCryptoData(cryptoId: string) {
+  try {
+    const response = await fetch(`/api/fetchCryptoData?cryptoID=${cryptoId}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch crypto data");
+    }
+    const data = await response.json();
+    return data.prices.map(([timestamp, price]: [number, number]) => ({
+      date: new Date(timestamp),
+      price: price,
+    }));
+  } catch (error) {
+    console.error("Error fetching crypto data:", error);
+    throw error;
+  }
+}
+
+export async function fetchCryptoList(page = 1, perPage = 50) {
+  try {
+    const response = await fetch(
+      `/api/fetchCryptoList?page=${page}&perPage=${perPage}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch crypto list");
+    }
+    const data = await response.json();
+    // console.log(data);
+    return data.map((crypto: any) => ({
+      id: crypto.id,
+      name: crypto.name,
+    }));
+  } catch (error) {
+    console.error("Error fetching crypto list:", error);
+    throw error;
+  }
+}
