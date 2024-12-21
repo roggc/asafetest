aed8fad0f31c36815b5a97fa9714ec7eba0c35f8
roggc <roggc9@gmail.com>
2024-12-21 04:16:36 +0100

    refactor

--- app/types/crypto.ts
+++ app/types/crypto.ts
@@ -5,23 +5,3 @@ export type Crypto = {
   image: string;
   current_price: number;
 };
-export type CryptoList = Crypto[];
-export type CryptoListResponse = {
-  data: Crypto[];
-};
-export type CryptoResponse = {
-  data: Crypto;
-};
-export type Error = {
-  error: string;
-};
-export type ErrorResponse = {
-  error: Error;
-};
-export type PaginatedList = {
-  data: CryptoList;
-  total: number;
-};
-export type PaginatedListResponse = {
-  data: PaginatedList;
-};
